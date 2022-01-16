c17f5b25a4afeea6b801abc4e9041ab76cc6177e
D3fau4 <raulwii2@gmail.com>
2022-01-16 13:44:56 +0100

    Agregar archivos de proyecto.

---
+++ 010EditorKeygen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _010EditorKeygen // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Number of users: 1000");
+            Console.WriteLine("Key: " + Keygen.MakeKey(username, 1000));
+        }
+    }
+}
