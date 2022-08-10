fd9d70728f7c9ba3de4328fc8fed190bcc325bc1
Raúl Álvarez <raulwii2@gmail.com>
2022-08-10 20:18:21 +0200

    add server

---
+++ EditorServer/Controllers/ServerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EditorServer.Controllers;
+
+
+[ApiController]
+[Produces("text/html")]
+[Route("cgibin/")]
+public class ServerController : ControllerBase
+{
+    [HttpGet("010editor_check_license_9b.php")]
+    public ContentResult CheckLicense()
+    {
+        return new ContentResult 
+        {
+            ContentType = "text/html",
+            Content = "<ss>valid</ss>"
+        };
+    }
+}
